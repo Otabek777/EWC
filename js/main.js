b866ddbc80415ab259279763bffdc7e62c8045b6
Otabek <53979293+Otabek777@users.noreply.github.com>
2024-08-18 03:35:41 +0500

    mobile

--- js/main.js
+++ js/main.js
@@ -1,15 +1,58 @@
+// paralax
 $(window).scroll(function() {
     let st = $(this).scrollTop();
-
     $(".banner__bg").css({
         "transform" : "translate(0px, " + st/5 + "px"
     });
 });
+
+// scroll header
 $(window).scroll(function(){
     // 1
     if ( $(this).scrollTop() > 55) {
         $('.header').addClass('active');
     } else {
         $('.header').removeClass('active');
+    };
+});
+
+// modal open
+$('.modal-open').click(function() {
+    $('.mainModal').addClass('active');
+    setTimeout(function() {
+        $('.mainModal').addClass('open');
+    },100);
+});
+
+// modal close
+$('.modal-close').click(function() {
+    $('.mainModal').removeClass('open');
+    setTimeout(function() {
+        $('.mainModal').removeClass('active');
+    },300);
+});
+
+// modal open time
+setTimeout(function() {
+    $('.mainModal').addClass('active');
+    setTimeout(function() {
+        $('.mainModal').addClass('open');
+    },100);
+},30000);
+
+// burgir
+$('.header__burgir .btn').click(function() {
+    if($('.header__burgir .btn').hasClass('active')) {
+        $('.header__burgir .btn').removeClass('rotate');
+        $('.header').removeClass('open');
+        setTimeout(function() {
+            $('.header__burgir .btn').removeClass('active');
+        }, 300);
+    } else {
+        $('.header__burgir .btn').addClass('active');
+        $('.header').addClass('open');
+        setTimeout(function() {
+            $('.header__burgir .btn').addClass('rotate');
+        }, 300);
     }
 });
